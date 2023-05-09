4c5de0cb8c545364ee807cd8983f7fd893f3ed17
robbyandrews <33102078+robbyandrews@users.noreply.github.com>
2023-05-09 07:27:31 -0700

    Update 2fa.sh

--- 2fa.sh
+++ 2fa.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# This command changes the PermitRootLogin option from the default of "without-password" to "Yes" to allow OTP authentication of the root user
 sed -i 's/^\(PermitRootLogin \).*/\1Yes/' /etc/ssh/sshd_config
+# These commands add the required options to allow for verifying using public key AND the OTP password method.
 sed -i '$a ChallengeResponseAuthentication yes'  /etc/ssh/sshd_config
 sed -i '$a AuthenticationMethods publickey,keyboard-interactive'  /etc/ssh/sshd_config
+# This command adds the Google PAM Library to the PAM config list and allows for OTPs using the Google Library
 sed -i '$a sshd-kbdint   auth required           /usr/local/lib/security/pam_google_authenticator.so' /etc/pam.conf
 
